e524acba3a7619649c383553d4569e86d35a3113
her-cat <i@her-cat.com>
2020-08-11 11:50:22 +0800

    add comment

--- chap-26/test_pthread.c
+++ chap-26/test_pthread.c
@@ -12,13 +12,20 @@ void *thread_run(void *arg) {
     }
 }
 
+/*
+ * pthread_exit(): 等待所有子线程终止，随后父线程也自己终止。
+ * pthread_cancel(): 主动终止某个线程。
+ * pthread_detach(): 分离某个线程，在子线程终止后能够自动回收相关的线程资源。
+ */
 int main(int argc, char *argv) {
     int calculator;
     pthread_t tid1, tid2;
 
+    /* 创建线程 */
     pthread_create(&tid1, NULL, thread_run, &calculator);
     pthread_create(&tid2, NULL, thread_run, &calculator);
 
+    /* 等待线程结束 */
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
 
